6d23b7b19b642acf4025140e497f0240281d161f
MichaCham <cham7694-github@yahoo.com>
2023-03-10 11:17:28 +0100

    premier Commit

---
+++ glouton.cpp
@@ -0,0 +1,5 @@
+//
+// Created by CHAMPALET on 10/03/2023.
+//
+
+#include "glouton.h"
---
+++ glouton.h
@@ -0,0 +1,14 @@
+//
+// Created by CHAMPALET on 10/03/2023.
+//
+
+#ifndef LIVRAISON_EDN_GLOUTON_H
+#define LIVRAISON_EDN_GLOUTON_H
+
+
+class glouton {
+
+};
+
+
+#endif //LIVRAISON_EDN_GLOUTON_H
